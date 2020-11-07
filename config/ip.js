adae94e5b4bf387ed39e316c79a1999a4bb95dd6
김동현 <donghyun.kim94@navercorp.com>
2020-11-07 18:59:20 +0900

    개발 편의를 위한 수정

--- config/ip.js
+++ config/ip.js
@@ -2,20 +2,22 @@ module.exports = function (defaultObj) {
   const request = require('request');
   const deasync = require('deasync');
 
-  (function () {
-    request("http://169.254.169.254/latest/meta-data/public-ipv4", function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        defaultObj.ipadd = body
-        if (defaultObj.ipadd == '54.180.24.79')
-          console.log(defaultObj.ipadd + ' 테스트 서버');
-        else
-          console.log(defaultObj.ipadd + ' 리얼 서버');
-      }
-    });
-    while (defaultObj.ipadd == undefined) {
-      deasync.runLoopOnce();
-    }
-  })();
+  // (function () {
+  //   request("http://169.254.169.254/latest/meta-data/public-ipv4", function (error, response, body) {
+  //     if (!error && response.statusCode == 200) {
+  //       defaultObj.ipadd = body
+  //       if (defaultObj.ipadd == '54.180.24.79')
+  //         console.log(defaultObj.ipadd + ' 테스트 서버');
+  //       else
+  //         console.log(defaultObj.ipadd + ' 리얼 서버');
+  //     }
+  //   });
+  //   while (defaultObj.ipadd == undefined) {
+  //     deasync.runLoopOnce();
+  //   }
+  // })();
+
+  defaultObj.ipadd = '54.180.24.79';
 
   return defaultObj;
 }
